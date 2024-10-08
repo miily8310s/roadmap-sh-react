377a2e5aa8a67afe805bde18fb76c82858064072
miruoon <rillakerika@gmail.com>
2024-10-08 09:36:00 +0900

    feat: changelog

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ChangeLog } from "./components/ChangeLog";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const LOG_DATA = [
+    {
+      id: "1",
+      date: "2023-02-28",
+      name: " AI Engineer Roadmap, Leaderboards, Editor AI, and more ",
+    },
+    {
+      id: "2",
+      date: "2023-02-28",
+      name: "テストテストテストテストテストああああああああああああああああ",
+    },
+  ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <h2>Changelog Component</h2>
+        <ChangeLog
+          datas={LOG_DATA}
+          title="Changelog"
+          description="We are always improving our content, adding new resources and adding features to enhance your learning experience. "
+        />
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
